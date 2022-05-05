4e6f6e1fda69cf4cdb3859c8873b120d6c7b091f
Thewoker <leoneljararavarotto@hotmail.com>
2022-05-05 15:18:13 -0300

    nuevos trabajos de la facultad

---
+++ Prog 3/Manejo de fechas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Manejo_de_fechas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
